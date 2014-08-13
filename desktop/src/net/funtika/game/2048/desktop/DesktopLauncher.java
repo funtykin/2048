5dc0cc820ccaa16d03bef84ec03627e16b055ba5
Sergey <funtykin@gmail.com>
2014-08-13 18:30:28 +0400

    first commit

---
+++ desktop/src/net/funtika/game/2048/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.funtika.game.2048.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.funtika.game.2048.MainGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGdx(), config);
+	}
+}
